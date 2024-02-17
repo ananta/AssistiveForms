997d9ec8388cdc11a905457294217aa7f54711ce
ananta <12180395+ananta@users.noreply.github.com>
2024-02-17 00:01:13 -0500

    chore(form-service): add random text generator util

---
+++ form-service/src/main/java/com/anntz/formservice/utils/RandomTextGenerator.java
@@ -0,0 +1,18 @@
+package com.anntz.formservice.utils;
+
+import java.util.Random;
+
+public class RandomTextGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String generateRandomText(int length) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
+}
