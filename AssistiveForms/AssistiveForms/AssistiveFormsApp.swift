c1cd332cc777bb930cee046b9baf97dbcff7ed37
ananta <12180395+ananta@users.noreply.github.com>
2024-02-24 15:22:15 -0500

    init(ios-app): bootstrap starter ios app

---
+++ AssistiveForms/AssistiveForms/AssistiveFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistiveFormsApp.swift
+//  AssistiveForms
+//
+//  Created by Ananta Bastola on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AssistiveFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
