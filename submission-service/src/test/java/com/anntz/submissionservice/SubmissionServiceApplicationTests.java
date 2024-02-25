b73b56c6844fb1bae4588e602aa3be23d5226722
ananta <12180395+ananta@users.noreply.github.com>
2024-02-25 09:24:11 -0500

    init(submission-service): bootstrap submission service using spring starter

---
+++ submission-service/src/test/java/com/anntz/submissionservice/SubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anntz.submissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
